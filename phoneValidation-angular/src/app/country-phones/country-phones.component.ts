54f8b8e9b99983d9b3ad38acf8c4efa1019013b6
bassemgawish <bassemgawish@hotmail.com>
2022-03-14 21:42:22 +0200

    Change select input options

--- phoneValidation-angular/src/app/country-phones/country-phones.component.ts
+++ phoneValidation-angular/src/app/country-phones/country-phones.component.ts
@@ -1,30 +1,29 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { environment } from 'src/environments/environment';
-import { CustomerSearch } from '../models/customer-search';
-import { Customer } from '../models/customer.model';
-import { CustomerService } from '../service/customer.service';
+import { HttpParams } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { environment } from "src/environments/environment";
+import { CustomerSearch } from "../models/customer-search";
+import { Customer } from "../models/customer.model";
+import { CustomerService } from "../service/customer.service";
 
 @Component({
-  selector: 'app-country-phones',
-  templateUrl: './country-phones.component.html',
-  styleUrls: ['./country-phones.component.css'],
+  selector: "app-country-phones",
+  templateUrl: "./country-phones.component.html",
+  styleUrls: ["./country-phones.component.css"],
 })
 export class CountryPhonesComponent implements OnInit {
   private readonly REST_API = environment.api_url;
   customerList: Customer[] = [];
 
   countries: string[] = [
-    '',
-    'Cameroon',
-    'Ethiopia',
-    'Morocco',
-    'Mozambique',
-    'Uganda',
+    "Cameroon",
+    "Ethiopia",
+    "Morocco",
+    "Mozambique",
+    "Uganda",
   ];
-  searchForm = new FormControl('');
-  search = new CustomerSearch('', '');
+  searchForm = new FormControl("");
+  search = new CustomerSearch("", "");
   constructor(private customerService: CustomerService) {}
 
   ngOnInit(): void {
@@ -39,14 +38,14 @@ export class CountryPhonesComponent implements OnInit {
 
   onSubmit() {
     let params = new HttpParams();
-    if (this.search.countryName != '') {
-      params = params.append('countryName', this.search.countryName);
+    if (this.search.countryName != "") {
+      params = params.append("countryName", this.search.countryName);
     }
-    if (this.search.state != '') {
-      params = params.append('state', this.search.state);
+    if (this.search.state != "") {
+      params = params.append("state", this.search.state);
     }
     this.customerService.filterCustomer(params).subscribe((data) => {
-      console.log('data recieved');
+      console.log("data recieved");
       this.customerList = data;
     });
     // this.httpClient
